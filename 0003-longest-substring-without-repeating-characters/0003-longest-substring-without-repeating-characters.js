e6c68d51ca5e20084ba831ece7522aa07750cb33
Liam Lindner <liamdw@hey.com>
2023-08-22 15:32:04 -0700

    Time: 79 ms (70.9%), Space: 44.4 MB (96.88%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    const seen = new Map();
+    let start = 0;
+    let maxLen = 0;
+
+    for(let i = 0; i < s.length; i++) {
+      if(seen.has(s[i])) {
+        start = Math.max(seen.get(s[i]) + 1, start)
+      }
+      seen.set(s[i], i);
+
+      maxLen = Math.max(i - start + 1, maxLen);
+    }
+
+    return maxLen;
+};
