bf392fb2c0d2e77b8232414b989ae6da3dc84333
Liam Lindner <liamdw@hey.com>
2023-08-31 22:00:07 -0700

    Time: 61 ms (35.89%), Space: 43.5 MB (79.24%) - LeetHub

---
+++ 0086-partition-list/0086-partition-list.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+var partition = function(head, x) {
+    let beforeStart = new ListNode(0);
+    let afterStart = new ListNode(0);
+
+    let before = beforeStart;
+    let after = afterStart;
+
+    while (head !== null) {
+        if (head.val < x) {
+            before.next = head;
+            before = before.next;
+        } else {
+            after.next = head;
+            after = after.next;
+        }
+        head = head.next;
+    }
+
+    after.next = null;
+
+    before.next = afterStart.next;
+
+    return beforeStart.next;
+
+};
