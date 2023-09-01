e5f9030dab3e3f6cf49ae65dee2d65133c72eb99
Liam Lindner <liamdw@hey.com>
2023-09-01 15:36:24 -0700

    Time: 382 ms (91.78%), Space: 152.1 MB (83.56%) - LeetHub

--- 2545-height-of-binary-tree-after-subtree-removal-queries/2545-height-of-binary-tree-after-subtree-removal-queries.js
+++ 2545-height-of-binary-tree-after-subtree-removal-queries/2545-height-of-binary-tree-after-subtree-removal-queries.js
@@ -18,16 +18,16 @@ var treeQueries = function(root, queries) {
   function getMaxHeight(node) {
     if (node === null) return 0;
     if (heights[node.val]) return heights[node.val];
-    
+
     heights[node.val] = 1 + Math.max(getMaxHeight(node.left), getMaxHeight(node.right));
     return heights[node.val];
   }
 
   function dfs(node, currentDepth, maxDepthWithoutCurrentNode) {
-    if (node === null) return 0;
-    
+    if (node === null) return;
+
     resultHeights[node.val] = maxDepthWithoutCurrentNode;
-    
+
     dfs(node.left, currentDepth + 1, Math.max(maxDepthWithoutCurrentNode, currentDepth + getMaxHeight(node.right)));
     dfs(node.right, currentDepth + 1, Math.max(maxDepthWithoutCurrentNode, currentDepth + getMaxHeight(node.left)));
   }
