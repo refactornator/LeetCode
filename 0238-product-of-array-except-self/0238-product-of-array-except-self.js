6d99bca11405b3bfa29a210739f7c63d611c27b2
Liam Lindner <liamdw@hey.com>
2023-10-02 11:29:04 -0700

    Time: 90 ms (51.43%), Space: 55 MB (38.87%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -3,29 +3,21 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    const n = nums.length;
-    const left = new Array(n).fill(1);
-    const right = new Array(n).fill(1);
-    const result = new Array(n);
+  let n = nums.length;
+  let result = new Array(n);
 
-    // Compute prefix products
-    let leftProduct = 1;
-    for (let i = 1; i < n; i++) {
-        leftProduct *= nums[i - 1];
-        left[i] = leftProduct;
-    }
+  // calculate prefix array
+  result[0] = 1;
+  for (let i = 1; i < n; i++) {
+    result[i] = result[i - 1] * nums[i - 1];
+  }
+  
+  // calculate suffix array
+  let right = 1;
+  for (let i = n - 1; i >= 0; i--) {
+    result[i] = result[i] * right;
+    right *= nums[i];
+  }
 
-    // Compute suffix products
-    let rightProduct = 1;
-    for (let i = n - 2; i >= 0; i--) {
-        rightProduct *= nums[i + 1];
-        right[i] = rightProduct;
-    }
-
-    // Compute result using left and right arrays
-    for (let i = 0; i < n; i++) {
-        result[i] = left[i] * right[i];
-    }
-
-    return result;
+  return result;
 };
